228470520cfc554ee685da95dc792fde12970ee3
Igor Chebotar <Wolk.fo@gmail.com>
2022-07-17 16:36:59 +0200

    1.1.6

--- Editor/Installer/Installer.cs
+++ Editor/Installer/Installer.cs
@@ -1,11 +1,19 @@
 using UnityEditor;
-using static SimpleMan.Utilities.Editor.EditorConstants;
+
 
 namespace SimpleMan.Utilities.Editor
 {
     [InitializeOnLoad]
     internal static class Installer
     {
+        //------FIELDS
+        public static string MAIN_FOLDER_PATH = "Assets/Plugins/SimpleMan/Utilities";
+        public static string MAIN_PACKAGE_PATH = "Assets/Plugins/SimpleMan/Utilities/Editor/Installer/MainPackage.unitypackage";
+        public static string ODIN_INSPECTOR_PATH = "Assets/Plugins/Sirenix/Odin Inspector";
+
+
+
+
         //------METHODS
         static Installer()
         {
